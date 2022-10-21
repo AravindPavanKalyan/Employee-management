8b532c29c9036869cffc247776e97d78ed5d7a77
Kalyan <aravindpavankalyan1996@gmail.com>
2022-10-21 16:05:57 +0530

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import { SignupComponent } from './authentication/components/signup/signup.compo
 import { ConceptsComponent } from './concepts/components/concepts.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { AuthGuard } from './shared/guards/auth.guard';
 import { UnitTestingDemoComponent } from './unit-testing-demo/components/unit-testing-demo/unit-testing-demo.component';
 
 const routes: Routes = [
   {path: '', component : HomeComponent},
   {path: 'concepts', component : ConceptsComponent},
-  {path: 'about', component : AboutComponent},
+  {path: 'about', component : AboutComponent, canActivate: [ AuthGuard ]},
   {path: 'unit-testing', component : UnitTestingDemoComponent},
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent}
 ];
