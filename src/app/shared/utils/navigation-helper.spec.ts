968324c86243a2aafd0a0297009f754ecdab2756
Kalyan <aravindpavankalyan1996@gmail.com>
2022-10-30 23:01:11 +0530

    navHelper spec

--- src/app/shared/utils/navigation-helper.spec.ts
+++ src/app/shared/utils/navigation-helper.spec.ts
@@ -1,10 +1,35 @@
+import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 import { NavigationHelper } from './navigation-helper';
 
-describe('NavigationHelper', () => {
-  let router: Router;
+// describe('NavigationHelper', () => {
+//   let router: Router;
 
-  it('should create an instance', () => {
-    expect(new NavigationHelper(router)).toBeTruthy();
+//   it('should create an instance', () => {
+//     expect(new NavigationHelper(router)).toBeTruthy();
+//   });
+// });
+
+describe('NavigationHelper', () => {
+  let navigationHelper: NavigationHelper;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ]
+    });
+    navigationHelper = TestBed.inject(NavigationHelper)
   });
+
+  it('should be created', () => {
+    expect(navigationHelper).toBeTruthy();
+  });
+
+  // testing navigateTo method
+  it('should navigate to the corresponding url given', () => {
+    spyOn( navigationHelper, 'navigateTo');
+    navigationHelper.navigateTo('/about'); // navigates to home page
+    expect(navigationHelper.navigateTo).toHaveBeenCalledWith('/about');
+  });
+
 });
